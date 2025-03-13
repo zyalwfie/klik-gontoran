4b8039d3f87faed7cd4fb480aeb63a139c1ce4e0
wafy <zyalwfie@gmail.com>
2025-03-13 20:27:02 +0800

    chore: set route for run the symlink command

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\FrontController;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [FrontController::class, 'index'])->name('home');
@@ -9,4 +10,8 @@ Route::get('/', [FrontController::class, 'index'])->name('home');
 Route::prefix('daftar-umkm')->group(function () {
     Route::get('/', [ProductController::class, 'index'])->name('product.index');
     Route::get('/{product:slug}', [ProductController::class, 'show'])->name('product.show');
+});
+
+Route::get('/run-symlink', function () {
+    Artisan::call('storage:link');
 });
